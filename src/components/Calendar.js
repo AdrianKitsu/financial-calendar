fd41e9442bdbb450927891fd34bed8283726c1b3
Adrian <aborges@trewknowledge.com>
2023-11-19 00:48:53 -0500

    initial calendar setup

---
+++ src/components/Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Calendar.module.css";
+
+function Calendar() {
+  return (
+    <div>
+      <div>Calendar</div>
+    </div>
+  );
+}
+
+export default Calendar;
